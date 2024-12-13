073d5762fd98b1babd13973658d8ddb02c11abb1
Samuel Lucky <samuelnoye35@gmail.com>
2024-12-13 10:42:04 +0100

    jpa-broiler

---
+++ jap/src/test/java/com/nobsafrica/jap/JapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobsafrica.jap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
